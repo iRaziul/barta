c9d7d82baa32080eca3ced91941116de85afa238
iRaziul <raziul.cse@gmail.com>
2026-09-18 20:31:51 +0600

    docs(facade): add method docblocks to Barta facade
    Add complete @method annotations with strict return types for IDE autocomplete and static analysis.

--- src/Facades/Barta.php
+++ src/Facades/Barta.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 namespace Larament\Barta\Facades;
 
+use Illuminate\Foundation\Bus\PendingDispatch;
 use Illuminate\Support\Facades\Facade;
 use Larament\Barta\BartaManager;
+use Larament\Barta\Data\ResponseData;
+use Larament\Barta\Drivers\AbstractDriver;
 
 /**
- * @method static self to(string|array $numbers)
- * @method static self message(string $message)
- * @method static \Larament\Barta\Data\ResponseData send()
- * @method static self driver(?string $driver)
+ * @method static AbstractDriver to(string|array<string> $numbers)
+ * @method static AbstractDriver message(string $message)
+ * @method static ResponseData send()
+ * @method static PendingDispatch queue(?string $queue = null, ?string $connection = null)
+ * @method static AbstractDriver reset()
+ * @method static string getName()
+ * @method static AbstractDriver driver(?string $driver = null)
+ * @method static string getDefaultDriver()
  *
  * @see BartaManager
+ * @see AbstractDriver
  */
 final class Barta extends Facade
 {
